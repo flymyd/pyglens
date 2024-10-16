39f907c50a288a891712c2fc2e3424ba4d283315
MYD\MYD-Work <2078546589@qq.com>
2024-10-16 11:27:45 +0800

    update

--- cv_crop.py
+++ cv_crop.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import requests
-from ultralytics import YOLO
 import tempfile
 
 
@@ -35,22 +34,11 @@ def cv_crop(image_path):
         print("Failed to load image.")
         return None
 
-    # 转换为灰度图像
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # 二值化处理
     _, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
-
-    # 找到非黑色区域的边界
     contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # 找到最大的轮廓
     max_contour = max(contours, key=cv2.contourArea)
-
-    # 获取最大轮廓的边界框
     x, y, w, h = cv2.boundingRect(max_contour)
-
-    # 裁剪图像
     cropped_image = image[y:y + h, x:x + w]
 
     with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as temp_file:
